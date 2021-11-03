515cb7b0b6620f3fa6f7346b1a88f533e053b318
Ahmad Elshowair <it.is.work@outlook.com>
2021-11-03 22:51:18 +0700

    add all folders and files for the website

---
+++ js/HomePlugIn.js
@@ -0,0 +1,23 @@
+﻿$(function()
+{
+    "use strict";
+    $(window).scroll(function(){
+        // window.console.log($(".navbar").height());
+        // window.console.log($(window).scrollTop());
+        var navbar = $(".navbar");
+        $(window).scrollTop() > $(".navbar").height() ? navbar.addClass("scrolled") : navbar.removeClass("scrolled");
+    });
+
+    $('.tab-switch li').click(function() {
+        $(this).addClass('active').siblings().removeClass('active');
+        // to hide first tab
+        $('.tabs-section .tab-content > div').hide();
+        // to show tab connect with link
+        $('.' + $(this).data('class')).show();
+    });
+
+    $("#go-up").click(function(){
+        "use strict";
+        $("html,body").animate({scrollTop: 0}, 1000);
+    });
+});
